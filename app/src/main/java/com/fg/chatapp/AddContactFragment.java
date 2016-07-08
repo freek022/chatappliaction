57e46d647c33df3359792e44788535b37b165631
freek022 <fkg333@gmail.com>
2016-07-08 17:53:26 +0100

    created the add contact class and its layout

---
+++ app/src/main/java/com/fg/chatapp/AddContactFragment.java
@@ -0,0 +1,18 @@
+package com.fg.chatapp;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddContactFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.add_contact_layout, null);
+    }
+}
